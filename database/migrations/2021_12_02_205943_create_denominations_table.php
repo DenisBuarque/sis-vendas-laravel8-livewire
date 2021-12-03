5ac01a6f64cdd29e9407074e7445d5cd7dcb8d8e
Denis Buarque <denisprogramadorweb@gmail.com>
2021-12-03 14:21:32 -0300

    first commit

---
+++ database/migrations/2021_12_02_205943_create_denominations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDenominationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('denominations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->enum('type', ['moeda','bilhete','outro']);
+            $table->string('value',50);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('denominations');
+    }
+}
---
+++ database/migrations/2021_12_02_211400_create_sales_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->double('total',8,2);
+            $table->integer('items');
+            $table->double('cash',8,2);
+            $table->double('change',8,2);
+            $table->enum('status', ['paid','pending','canceled']);
+            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
